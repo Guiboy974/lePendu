44fc4e86aaec1199c9174614400014a8043c0ab8
Guiboy974 <139962386+Guiboy974@users.noreply.github.com>
2024-11-18 11:59:07 +0100

    affiché lettre ok

--- lePendu.js
+++ lePendu.js
@@ -1,29 +1,26 @@
 "use strict"
 
-const tabMot = ["DEVELOPPEUR", "INTEGRATEUR", "CLIENT", "SERVEURS", "JAVASCRIPT", "REACTJS", "SYMFONY", "MYSQL", "BOOTSTRAP", "NAVIGATEUR", "INTERNET", "FRAMEWORK", "TAILWIND", "MOZILLA", "CHROME", "FREELANCE", "CONCEPTEUR", "APPLICATION","MERISE","HTML","STYLE","TERMINAL"];
+const tabMot = ["DEVELOPPEUR", "INTEGRATEUR", "CLIENT", "SERVEURS", "JAVASCRIPT", "REACTJS", "SYMFONY", "MYSQL", "BOOTSTRAP", "NAVIGATEUR", "INTERNET", "FRAMEWORK", "TAILWIND", "MOZILLA", "CHROME", "FREELANCE", "CONCEPTEUR", "APPLICATION", "MERISE", "HTML", "STYLE", "TERMINAL"];
 
-const motAlea = tabMot[Math.floor(Math.random()*tabMot.length)];
+// génère un mot aleatoire a partir du tableau
+const motAlea = tabMot[Math.floor(Math.random() * tabMot.length)];
 console.log(motAlea);
 
-const fieldText = document.getElementById("alphabet") 
+const fieldText = document.getElementById("alphabet")
 const showLetter = document.getElementById("try_show");
 const tries = document.getElementById("try_list");
 let letterToCompare;
 
+// récupère la lettre cliquer
 function pickLetter(event) {
     if (event.target.tagName === "P") {
         letterToCompare = event.target.textContent;
     }
 }
 
-function compare() {
-    const matches = motAlea.match(letterToCompare);
-    console.log(matches);
-
-}
-
+// affiche l'emplacement du mot a trouvé en cachant le mot
 function displayLocation() {
-    for (let i = 0; i < motAlea.length -1; i++) {
+    for (let i = 0; i < motAlea.length; i++) {
         const letterSpan = document.createElement("span");
         letterSpan.textContent = motAlea[i]
         letterSpan.classList.add("letter-span");
@@ -31,6 +28,17 @@ function displayLocation() {
     }
 }
 displayLocation();
+
+// compare la lettre cliquer au mot a trouver et l'affiche
+function compare() {
+    for (let i = 0; i < motAlea.length; i++) {
+        if (motAlea[i] == letterToCompare) {
+            console.log(motAlea[i]);
+            const letterSpan = showLetter.getElementsByTagName("span")[i]
+            letterSpan.classList.add("letter-ok");
+        }
+    }
+}
 
 fieldText.addEventListener("click", pickLetter)
 fieldText.addEventListener("click", compare)
