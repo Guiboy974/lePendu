b3da3342366c52adbade7fdd6d8951724abfc692
Guiboy974 <139962386+Guiboy974@users.noreply.github.com>
2024-11-19 14:30:23 +0100

    modif tab

--- lePendu.js
+++ lePendu.js
@@ -1,6 +1,7 @@
 "use strict"
 
-import { tabMot } from "./tab.js";
+// import { tabMot } from "./tab.js";
+const tabMot = ["DEVELOPPEUR", "INTEGRATEUR", "CLIENT", "SERVEURS", "JAVASCRIPT", "REACTJS", "SYMFONY", "MYSQL", "BOOTSTRAP", "NAVIGATEUR", "INTERNET", "FRAMEWORK", "TAILWIND", "MOZILLA", "CHROME", "FREELANCE", "CONCEPTEUR", "APPLICATION", "MERISE", "HTML", "STYLE", "TERMINAL", "ANGULAR","GITHUB","GITLAB","DOCKER","INFORMATIQUE","SVELTE",];
 
 // génère un mot aleatoire a partir du tableau
 function newAlea(tab) {
